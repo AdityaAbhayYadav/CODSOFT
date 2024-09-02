66548267265365e7bda217f17b1b7685afd06443
Aditya Abhay Yadav <112894429+AdityaAbhayYadav@users.noreply.github.com>
2024-09-02 17:29:42 +0530

    Add files via upload
    completed  python file

---
+++ bool datatype.py
@@ -0,0 +1,31 @@
+# Without using bool() function.
+'''
+x = 6
+y = 4
+print(x>y)
+'''
+
+# With using bool() function.
+'''
+x= 6
+y= 4
+print(bool(x==y))
+'''
+
+# Integers and Floats as Boolean.
+'''1'''
+x=0
+print(bool(x))
+
+'''2'''
+x=5
+print(bool(x))
+
+'''3'''
+x=9.7
+print(bool(x))
+
+'''4'''
+x=-9.7
+print(bool(x))
+
